e50503a0a4567ee58d8bbae02e3af41d1f11bd3b
zuken321 <mr.kostya00@gmail.com>
2019-05-23 15:22:59 +0500

    fix posts

--- views/site/posts.php
+++ views/site/posts.php
@@ -6,13 +6,16 @@ if(!Yii::$app->user->isGuest)
     echo '<a href="/new_post">Добавить пост</a>';
 }
 ?>
-<?php foreach ($posts as $post):?>
-<div class="post">
-    <a href="/posts/<?=Html::encode($post->post_id)?>">
-        <h2><?= Html::encode($post->title)?></h2>
-        <span><?= Html::encode($post->users->username)?></span>
-        <p><?= Html::encode($post->short_text)?></p>
-    </a>
-</div>
-
-<?php endforeach;?>
+<?php if(count($posts)):?>
+    <?php foreach ($posts as $post):?>
+    <div class="post">
+        <a href="/posts/<?=Html::encode($post->post_id)?>">
+            <h2><?= Html::encode($post->title)?></h2>
+            <span><?= Html::encode($post->users->username)?></span>
+            <p><?= Html::encode($post->short_text)?></p>
+        </a>
+    </div>
+    <?php endforeach;?>
+<?php else:?>
+    <p>Нет постов</p>
+<?php endif;?>
