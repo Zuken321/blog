c9919da74f04cc57b001469033606f275ea8441d
zuken321 <mr.kostya00@gmail.com>
2019-05-24 12:50:16 +0500

    Логика из PostController вынесена в PostsTable

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -2,62 +2,43 @@
 
 namespace app\controllers;
 
-use app\models\CommentsTable;
 use Yii;
 use yii\web\Controller;
 use app\models\PostForm;
 use app\models\PostsTable;
 use yii\data\ArrayDataProvider;
-use app\models\CommentForm;
 
 class PostController extends Controller
 {
     public function actionIndex()
     {
-        $posts = PostsTable::find()->orderBy('post_id DESC')->all();
-        $posts_provider =  new ArrayDataProvider([
-            'allModels' => $posts,
-            'pagination' => [
-                'pageSize' => 10,
-            ],
-        ]);
-        return $this->render('index', compact('posts_provider'));
+        $post_table = new PostsTable();
+        $posts = $post_table->getPosts();
+        return $this->render('index', $posts);
     }
 
     public function actionPost($post_id)
     {
-        if(isset($post_id)) {
-            $count_post = PostsTable::find()->where(['post_id' => $post_id])->count();
-            if($count_post != 0) {
-                $post = PostsTable::findOne($post_id);
+        if (isset($post_id)) {
+            $post_table = new PostsTable();
+            $post = $post_table->getPost($post_id);
 
-                $comments = CommentsTable::find()->orderBy('comment_id DESC')->all();
-                $comments_provider =  new ArrayDataProvider([
-                    'allModels' => $comments,
-                    'pagination' => [
-                        'pageSize' => 10,
-                    ],
-                ]);
-
-                $model = new CommentForm();
-                return $this->render('post', compact('post', 'comments_provider', 'model'));
-            } else {
-                return Yii::$app->response->redirect('/posts');
+            if ($post != false) {
+                return $this->render('post', $post);
             }
-        } else {
-            return Yii::$app->response->redirect('/posts');
         }
+        return Yii::$app->response->redirect('/posts');
     }
 
     public function actionNewPost()
     {
-        if(Yii::$app->user->isGuest) {
+        if (Yii::$app->user->isGuest) {
             return Yii::$app->response->redirect('/posts');
         }
         $post_form = new PostForm();
-        if($post_form->load(Yii::$app->request->post()) && $post_form->addPost($post_form)) {
+        if ($post_form->load(Yii::$app->request->post()) && $post_form->addPost($post_form)) {
             return Yii::$app->response->redirect('/posts');
         }
-        return $this->render('newPost', compact('post_form'));
+        return $this->render('newPost', compact('post_form')); // надо обработать ошибку при некорректных данных
     }
 }
--- models/PostsTable.php
+++ models/PostsTable.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\models;
 
+use Yii;
+use yii\data\ArrayDataProvider;
 use yii\db\ActiveRecord;
 
 class PostsTable extends ActiveRecord
@@ -13,6 +15,38 @@ class PostsTable extends ActiveRecord
     public function getUsers()
     {
         return $this->hasOne(User::className(), ['user_id' => 'author_id']);
+    }
+
+    public function getPost($post_id)
+    {
+        $count_post = $this->find()->where(['post_id' => $post_id])->count();
+        if ($count_post != 0) {
+            $post = $this->findOne($post_id);
+
+            $comments = CommentsTable::find()->where(['post_id' => $post_id])->orderBy('comment_id DESC')->all();
+            $comments_provider =  new ArrayDataProvider([
+                'allModels' => $comments,
+                'pagination' => [
+                    'pageSize' => 10,
+                ],
+            ]);
+
+            $model = new CommentForm();
+            return compact('post', 'comments_provider', 'model');
+        }
+        return false;
+    }
+
+    public function getPosts()
+    {
+        $posts = $this->find()->orderBy('post_id DESC')->all();
+        $posts_provider =  new ArrayDataProvider([
+            'allModels' => $posts,
+            'pagination' => [
+                'pageSize' => 10,
+            ],
+        ]);
+        return compact('posts_provider');
     }
 }
 ?>
